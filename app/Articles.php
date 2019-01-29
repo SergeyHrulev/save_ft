6282d0366f7daab40634db7d1f0e043fc6053281
SergeyHrulev <sergey.hrulev@gmail.com>
2019-01-29 19:47:00 +0300

    add antracked files

--- app/Articles.php
+++ app/Articles.php
@@ -4,10 +4,16 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\MediaLibrary\Models\Media;
+use Spatie\MediaLibrary\HasMedia\HasMedia;
+use Spatie\MediaLibrary\HasMedia\HasMediaTrait;
+use Spatie\MediaLibrary\File;
 
-class Articles extends Model
+class Articles extends Model implements HasMedia
 {
     use SoftDeletes;
+    use HasMediaTrait;
+
     //
     protected $fillable = [
         'meta_title',
@@ -35,5 +41,28 @@ class Articles extends Model
 
     public function photo(){
         return $this->hasMany(ArticlePhoto::class);
+    }
+
+    /*
+     * @return media collection
+     */
+
+    public function registerMediaCollections()
+    {
+        // TODO: Implement registerMediaCollections() method.
+        $this->addMediaCollection('article-photo');
+        $this->addMediaCollection('article-text-photo');
+    }
+
+    /*
+     * @return thumb img
+     */
+
+    public function registerMediaConversions(Media $media = null)
+    {
+        // TODO: Implement registerMediaConversions() method.
+        $this->addMediaConversion('thumb')
+            ->width(300)
+            ->height(180);
     }
 }
